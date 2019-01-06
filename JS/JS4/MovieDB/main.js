60c6a738eaaa3877847859079bad1c47d5b9076a
Oleksandr Kononenko <s_com@ukr.net>
2019-01-06 12:50:31 +0200

    svg, rating, genres, search movie page onclick

--- JS/JS4/MovieDB/main.js
+++ JS/JS4/MovieDB/main.js
@@ -7,14 +7,15 @@ var storage = {
     articleListLength: '',
     limit: 10,
 
-    articleList: [],        // получаем массив объектов
-    movieIdClicked: '',     // один элемент
+    articleList: [],        // получаем "главный"  массив объектов, оно же data, storage.articleList
+    movieIdClicked: '',     // один элемент, одного из объектов из массива storage.articleList
 
-    movieItem: {},        // один из объектов из другого api
+    movieItem: {},        // один из объектов(фильм по id) из другого API movie/search
 
     movieList: [],      // массив объектов для списка поиска
     list : document.getElementById('listM'),
     movieBackground: document.getElementById('movieBackground'),
+    storageRun: null
 };
 
 // function loadConsoleStorageArticleList() {
@@ -24,11 +25,7 @@ var storage = {
 // function loadConsoleStorageMovieItem() {
 //     console.log(storage.movieItem);
 // }
-// ---------------------- header -----------------------------
 
-
-
-// ----------------------------- end header -------------------------------------------------------------
 // ---------------- 1 -----------------
 window.onload = function () {
     render()
@@ -166,7 +163,7 @@ function ArticalItem (data) {
     cardBodyDiv.className = 'card-body';
     h4.className = 'card-title';
     p.className = 'card-text';
-    let idMovie = data.id
+    let idMovie = data.id;
     a.id = idMovie;
 
     moreFooterDiv.className = 'card-footer';
@@ -189,9 +186,9 @@ function ArticalItem (data) {
 
     a.innerText = 'Подробнее';
     let id = storage.articleList.id;
-    a.addEventListener('click',         // ссылка чистит
-        (function(){clearList('movie')})
-    );
+    // a.addEventListener('click',         // ссылка чистит
+    //     (function(){clearList('movie')})
+    // );
     a.addEventListener('click',
         (function(){clearList('articleList')})
     );
@@ -208,9 +205,10 @@ function ArticalItem (data) {
     );
 
     a.addEventListener('click',
-
         renderMovie
-
+    );
+    a.addEventListener('click',
+        storage.storageRun
     );
     // a.removeEventListener(click, renderMovie ());
 
@@ -240,7 +238,7 @@ function ArticalItem (data) {
 //let movie_id = storage.articleList.id
 //--------------------------  Movies List page
 function renderMovie () {
-  //  getTopMovie ()
+    //  getTopMovie ()
     getData('movie/'+ storage.movieIdClicked, '')
         .then(function(resM) {
             return resM.json()
@@ -300,18 +298,37 @@ function MovieItem () {
     // console.log(imgAbout);
     let imgAboutSrc = document.createElement('img');
     let textAbout = document.createElement('div');
+    let iconsAbout = document.createElement('div');
     let pTextAbout = document.createElement('p');
+    let genresAboutUl = document.createElement('ul');
+    let genresAboutLi;
+
+
     // ------------------добавили HTML class для дальнейшей стилизации
     itemMovie.className = 'card-title';
     imgAbout.className = 'col-12 col-sm-4 col-md-4 col-lg-5 col-xl-4';
     textAbout.className = 'col-12 col-sm-8 col-md-8 col-lg-7 col-xl-8 d-flex flex-column ';
-    imgAboutSrc.className = 'imgAboutSrc'
+    imgAboutSrc.className = 'imgAboutSrc';
+    iconsAbout.className = 'icons ';
+
+
     // -------------------определяем наполнение этих элементов из API
 
+    iconsAbout.innerHTML = '<div class="ratingText">Рейтинг пользователя</div>';
     itemMovie.innerText = storage.movieItem.title;
     imgAboutSrc.src = 'https://image.tmdb.org/t/p/w500' + storage.movieItem.poster_path;
-    pTextAbout.innerText = storage.movieItem.overview;
- //   back_poster.src = 'https://image.tmdb.org/t/p/w500' + storage.movieItem.backdrop_path;
+    pTextAbout.innerHTML = '<h4>Review</h4>'+ storage.movieItem.overview;
+
+
+
+    // genresAboutLi.innerHTML = Object.values(storage.movieItem.genres.name).forEach(function(item) {
+    //     genresAboutLi.appendChild(genresAboutUl)
+    // })
+
+
+    //   back_poster.src = 'https://image.tmdb.org/t/p/w500' + storage.movieItem.backdrop_path;
+
+
     // -------------------------Выводим элементы на страницу
 
     //imgAbout.appendChild(imgAboutSrc);
@@ -320,15 +337,173 @@ function MovieItem () {
     imgAbout.appendChild(imgAboutSrc);
     movie.appendChild(textAbout);
     textAbout.appendChild(itemMovie);
+    textAbout.appendChild(iconsAbout);
     textAbout.appendChild(pTextAbout);
 
-   storage.movieBackground.style.display = 'block';
+    textAbout.appendChild(genresAboutUl);      // для жанров
+    // genresAboutUl.appendChild(genresAboutLi);
+    storage.movieItem.genres.forEach(function(item) {
+        genresAboutLi = document.createElement('li');
+        genresAboutLi.innerHTML = item.name;            // перезатирает каждый раз
+        genresAboutLi.id = item.id;
+        genresAboutLi.className= 'genre badge badge-info';
+        genresAboutUl.appendChild(genresAboutLi);
+    });
+
+
+
+    storage.movieBackground.style.display = 'block';
     storage.movieBackground.style.background = `url('https://image.tmdb.org/t/p/w500${storage.movieItem.backdrop_path}') no-repeat`;
     storage.movieBackground.style.backgroundSize = "cover";
-   // movieBackground.style.backgroundColor
+    // movieBackground.style.backgroundColor
     //--------------------------  Infinite scroll  > render() while limit allows
     window.onscroll = function() {
-       return null;
+        return null;
     };
     storage.list.style.display = 'none';
+
+
+    let roundRating = document.getElementById('score-rating');
+    roundRating.innerText =  storage.movieItem.vote_average*10 + '%';
+
+    // ----------------------------------- SVG begin---------------------------------------
+    // DOM Elements
+
+    const button = document.querySelector('js-button')
+    const circle = document.getElementById('js-circle')
+    const text = document.getElementById('score-rating')
+
+
+// Circle radius, diameter and offset function
+
+    const radius = circle.getAttribute('r')
+    const diameter = Math.round(Math.PI * radius * 2)
+    const getOffset = (val = 0) => Math.round((100 - val) / 100 * diameter)
+
+
+// Generate random number and set offset and percentage
+    let val = storage.movieItem.vote_average*10;
+console.log(val);
+    let run = () => {
+        // const val = Math.floor(Math.random() * 100)
+        circle.style.strokeDashoffset = getOffset(val)
+        text.textContent = `${val}%`
+    }
+run();
+// storage.storageRun = run;
+// Event listeners
+
+    // button.addEventListener('click', run);
+    document.addEventListener('DOMContentLoaded', () => setTimeout(run, 10))
+
+    // ----------------------------------- SVG end --------------------------------------
 }
+
+
+// ---------------------- header -----------------------------
+// ---------------------- header -----------------------------
+function searchMovie () {
+    return fetch(
+        'https://api.themoviedb.org/3/search/movie?api_key=1078453dc71a614c3a03d74c27fbdcb1&language=en-US&page=1&include_adult=false&query=' + input.value,
+        {
+            method: 'GET'
+        }
+    ).then(function(res) { return res.json()})
+}
+///movie/top_rated API
+
+// -------------------------- Sarch Movies ---------------------------------------------------------
+let input = document.getElementById('searchMovie');
+
+input.oninput = function() {
+    clearSearch('searchList')
+    searchMovie()
+        .then(function(res) {
+            console.log(res)
+            storage.movieList = res.results
+            searchList.appendChild(MovieList())
+        })
+};
+
+
+function clearSearch(el){
+    document.getElementById(el).innerHTML = "";
+}
+
+function MovieList () {
+    var div = document.createElement('div');
+    div.className = 'MovieList';
+    if (input.value) {
+        storage.movieList.forEach(function (item) {
+            div.appendChild(ItemMovieFromList(item));
+        })
+    }
+    return div
+}
+
+
+function ItemMovieFromList(data) {
+    var searchList = document.createElement('div');
+
+    var searchListUl = document.createElement('ul');
+    var searchListLi = document.createElement('li');
+
+
+    var aSearch = document.createElement('a');
+
+
+
+    searchList.className = 'col-12 col-sm-12 col-md-8 input-group ';
+
+    searchListUl.className = 'suggests-component';
+
+
+
+
+    aSearch.className = 'ItemMovieFromSearchList';
+    aSearch.innerText = data.title;
+
+
+    searchList.appendChild(searchListUl);
+    searchListUl.appendChild(aSearch);
+    aSearch.appendChild(searchListLi);
+    let idMovie = data.id;
+    aSearch.id = idMovie;
+
+    let id = storage.articleList.id;
+    // a.addEventListener('click',         // ссылка чистит
+    //     (function(){clearList('movie')})
+    // );
+    aSearch.addEventListener('click',
+        (function(){clearList('articleList')})
+    );
+    aSearch.addEventListener('click',
+        (function(){clearList('searchList')})
+    );
+    aSearch.addEventListener('click',
+        (function(){clearList('searchMovie')})
+    );
+    aSearch.addEventListener('click',
+
+        (function() {
+            // storage.movieIdClicked = this.backdrop_path.substring(1);
+            storage.movieIdClicked = this.id;
+            console.log(storage.movieIdClicked);
+            console.log(storage.movieItem)
+        })
+
+    );
+
+    aSearch.addEventListener('click',
+        renderMovie
+    );
+    aSearch.addEventListener('click',
+        storage.storageRun
+    );
+
+    return searchList
+}
+
+
+
+// ----------------------------- end header -------------------------------------------------------------
